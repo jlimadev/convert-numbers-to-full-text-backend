e01738f87b38108f9d369748702a3ef20615e0fe
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-05-27 15:48:32 -0300

    chore(dependencies) adjust dependencies

---
+++ jest.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+
+  modulePathIgnorePatterns: ['<rootDir>/coverage/'],
+
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/coverage/',
+    '<rootDir>/dist/',
+  ],
+
+  testMatch: ['<rootDir>/**/*.spec.ts'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/coverage/',
+    '<rootDir>/dist/',
+  ],
+
+  watchPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/coverage/',
+    '<rootDir>/dist/',
+  ],
+
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.ts', '!**/jest.config.js', '!**/debug.js', '!**/run.ts'],
+  coverageDirectory: './coverage',
+  coverageReporters: ['lcov', 'text'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
