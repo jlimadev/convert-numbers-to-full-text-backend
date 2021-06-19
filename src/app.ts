658ff44102d43170956556ba80f553b0c7680d9f
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-06-19 09:54:36 -0300

    build(app): add start script

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import { writtenNumbersData } from './core/domain/data/NumbersData';
+import { WrittenNumberFactory } from './core/usecases/number-factory/WrittenNumberFactory';
+import { SpellOutNumber } from './core/usecases/spell-out-number/SpellOutNumber';
+
+(() => {
+  const numbersFactory = new WrittenNumberFactory(writtenNumbersData);
+  const writeNumbers = new SpellOutNumber(numbersFactory);
+
+  const numbers = Array.from(Array(999).keys());
+  const writtenNumbers = numbers.map((number) =>
+    writeNumbers.invokeWith(number),
+  );
+
+  console.log(writtenNumbers);
+})();
