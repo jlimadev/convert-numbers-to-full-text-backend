b18379d8a8b7ebee83fc33595609c2bf0e178d27
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-11-17 17:01:01 -0300

    feature(main): show reverse

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,10 @@ import { SpellOutNumber } from './core/usecases/spell-out-number/SpellOutNumber'
   const numbersFactory = new WrittenNumberFactory(writtenNumbersData);
   const writeNumbers = new SpellOutNumber(numbersFactory);
 
-  const numbers = Array.from(Array(999).keys());
+  const numbers = Array.from(Array(10000).keys());
   const writtenNumbers = numbers.map((number) =>
     writeNumbers.invokeWith(number),
   );
 
-  console.log(writtenNumbers);
+  console.log(writtenNumbers.reverse());
 })();
