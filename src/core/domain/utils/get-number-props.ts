a829260c6fd16d10bcaa97e7257c85f8d2087c75
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-11-19 16:52:31 -0300

    refactor(get-number-props): change it to return more data

--- src/core/domain/utils/get-number-props.ts
+++ src/core/domain/utils/get-number-props.ts
@@ -1,17 +1,25 @@
 type Props = {
   length: number;
-  roundedNumber: number;
-  rest: number;
+  numberUnit: number;
+  numberRoundTen: number;
+  numberRoundHundred: number;
+  numberRoundThousand: number;
+  numberRestDigits: number;
 };
 
 export class GetNumberProps {
-  static get(number: number, multiplier: number): Props {
+  static get(number: number): Props {
     const numberAsString = number.toString();
     const length = numberAsString.length;
-    const firstDigit = numberAsString.charAt(0);
-    const restDigits = numberAsString.substring(1, length);
-    const roundedNumber = Number(firstDigit) * multiplier;
-    const rest = Number(restDigits);
-    return { length, roundedNumber, rest };
+    const firstDigit = Number(numberAsString.charAt(0));
+    const restDigits = Number(numberAsString.substring(1, length));
+    return {
+      length: length,
+      numberUnit: firstDigit,
+      numberRoundTen: firstDigit * 10,
+      numberRoundHundred: firstDigit * 100,
+      numberRoundThousand: firstDigit * 1000,
+      numberRestDigits: restDigits,
+    };
   }
 }
