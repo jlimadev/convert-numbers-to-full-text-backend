35e9512ac4e482d0245a8400c5bed888df1fbf7d
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-11-19 18:35:03 -0300

    test(controller): add makesut

--- src/presentation/controllers/convert-number-to-full-text-controller.spec.ts
+++ src/presentation/controllers/convert-number-to-full-text-controller.spec.ts
@@ -1,12 +1,17 @@
 import { ConvertNumberToFullTextController } from './convert-number-to-full-text-controller';
 import { ConvertNumberToFullText } from '../../core/domain/protocols';
 
+const makeSut = () => {
+  const convertor: ConvertNumberToFullText = {
+    invoke: jest.fn(),
+  };
+  const sut = new ConvertNumberToFullTextController(convertor);
+  return { sut, convertor };
+};
+
 describe('ConvertNumberToFullTextController', () => {
   it('should call usecase with correct number', () => {
-    const convertor: ConvertNumberToFullText = {
-      invoke: jest.fn(),
-    };
-    const sut = new ConvertNumberToFullTextController(convertor);
+    const { sut, convertor } = makeSut();
     const number = 10;
     sut.invoke(number);
     expect(convertor.invoke).toHaveBeenCalledWith(number);
