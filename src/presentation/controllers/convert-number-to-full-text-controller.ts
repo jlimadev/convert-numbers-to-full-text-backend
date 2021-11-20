be830f82b84d373d9a8b5ebd8df70639c749ed03
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-11-20 09:13:27 -0300

    feat(controller): ensure it returns internal server error if usecase throws

--- src/presentation/controllers/convert-number-to-full-text-controller.ts
+++ src/presentation/controllers/convert-number-to-full-text-controller.ts
@@ -1,14 +1,18 @@
 import { ConvertNumberToFullText } from '../../core/domain/protocols';
-import { HttpResponse, notFound, ok } from '../protocols';
+import { HttpResponse, notFound, ok, internalServerError } from '../protocols';
 
 export class ConvertNumberToFullTextController {
   constructor(private readonly convertor: ConvertNumberToFullText) {}
 
   handle(number: number): HttpResponse {
-    const fullText = this.convertor.invoke(number);
-    if (!fullText) {
-      return notFound(`Could not find ${number} as full text`);
+    try {
+      const fullText = this.convertor.invoke(number);
+      if (!fullText) {
+        return notFound(`Could not find ${number} as full text`);
+      }
+      return ok(fullText);
+    } catch (error) {
+      return internalServerError(error);
     }
-    return ok(fullText);
   }
 }
