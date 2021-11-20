bdd9b5319b5f45291ab2d4e2bff3a03d51bad5b6
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-11-20 08:54:03 -0300

    test(controller): ensure it returns notFound if usecase returns null

--- src/presentation/controllers/convert-number-to-full-text-controller.spec.ts
+++ src/presentation/controllers/convert-number-to-full-text-controller.spec.ts
@@ -1,9 +1,10 @@
 import { ConvertNumberToFullTextController } from './convert-number-to-full-text-controller';
 import { ConvertNumberToFullText } from '../../core/domain/protocols';
+import { notFound } from '../protocols';
 
 const makeSut = () => {
   const convertor: ConvertNumberToFullText = {
-    invoke: jest.fn(),
+    invoke: jest.fn().mockReturnValue('any-number-as-full-text'),
   };
   const sut = new ConvertNumberToFullTextController(convertor);
   return { sut, convertor };
@@ -13,7 +14,14 @@ describe('ConvertNumberToFullTextController', () => {
   it('should call usecase with correct number', () => {
     const { sut, convertor } = makeSut();
     const number = 10;
-    sut.invoke(number);
+    sut.handle(number);
     expect(convertor.invoke).toHaveBeenCalledWith(number);
+  });
+  it('should return 404 notFound if use case returns null', () => {
+    const { sut, convertor } = makeSut();
+    jest.spyOn(convertor, 'invoke').mockReturnValueOnce(null);
+    const number = 10;
+    const result = sut.handle(number);
+    expect(result).toEqual(notFound(`Could not find ${number} as full text`));
   });
 });
