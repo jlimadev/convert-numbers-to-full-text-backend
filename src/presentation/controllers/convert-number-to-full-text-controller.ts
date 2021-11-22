6495ce3c2c2567fd9809a29944eb797a47d1ca2a
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-11-22 09:12:15 -0300

    refactor(presentation): change controller implementation

--- src/presentation/controllers/convert-number-to-full-text-controller.ts
+++ src/presentation/controllers/convert-number-to-full-text-controller.ts
@@ -1,16 +1,21 @@
 import { ConvertNumberToFullText } from '../../core/domain/protocols';
-import { HttpResponse, notFound, ok, internalServerError } from '../protocols';
+import { HttpResponse, notFound, ok, internalServerError, HttpRequest, badRequest } from '../http';
+import { Controller, Validator } from '../protocols';
 
-export class ConvertNumberToFullTextController {
-  constructor(private readonly convertor: ConvertNumberToFullText) {}
+export class ConvertNumberToFullTextController implements Controller {
+  constructor(
+    private readonly numberValidator: Validator,
+    private readonly convertor: ConvertNumberToFullText,
+  ) {}
 
-  handle(number: number): HttpResponse {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const error = this.numberValidator.validate(httpRequest.params);
+      if (error) return badRequest(error);
+      const number = Number(httpRequest.params.number);
       const fullText = this.convertor.invoke(number);
-      if (!fullText) {
-        return notFound(`Could not find ${number} as full text`);
-      }
-      return ok(fullText);
+      if (!fullText) return notFound(number);
+      return ok({ number: fullText });
     } catch (error) {
       return internalServerError(error);
     }
--- src/presentation/http/http-response.ts
+++ src/presentation/http/http-response.ts
@@ -1,4 +1,4 @@
 export type HttpResponse = {
   statusCode: number;
-  body: string | Error;
+  body: any;
 };
