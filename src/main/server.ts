4098eab943f834bc06a71fb3b94f7fedbea658ca
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-11-22 10:54:24 -0300

    refactor(main): add server port as environment variable

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,6 @@
 import app from './config/app';
+import env from './config/env';
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000!');
+app.listen(env.port, () => {
+  console.log(`Server started on port ${env.port}!`);
 });
